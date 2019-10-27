3826d2fad6d05062af861129f03f3ab1fa80574e
Przemyslaw Gornik <gornik.przemek@gmail.com>
2019-10-27 14:27:32 +0100

    Dodanie klasy DataRepository oraz metod add.. i getAll..

---
+++ Zadanie1/DataRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class DataRepository
+    {
+        private DataContext dataContext;
+        private DataFiller dataFiller;
+
+        public DataRepository(DataContext dataContext, DataFiller dataFiller)
+        {
+            this.dataContext = dataContext;
+            this.dataFiller = dataFiller;
+        }
+
+
+        // metody getAll...
+        public IEnumerable<Katalog> getAllKatalog()
+        {
+            return dataContext.katalogi.Values;
+        }
+
+        public IEnumerable<Wykaz> getAllWykaz()
+        {
+            return dataContext.czytelnicy;
+        }
+
+        public IEnumerable<Zdarzenie> getAllZdarzenie()
+        {
+            return dataContext.zdarzenie;
+        }
+
+        public IEnumerable<OpisStanu> getAllOpisStanu()
+        {
+            return dataContext.egzemplarze;
+        }
+
+        //metody add...
+
+        public void addWykaz(Wykaz element)
+        {
+            dataContext.czytelnicy.Add(element);
+        }
+
+        public void addKatalog(Katalog pozycja)
+        {
+            dataContext.katalogi.Add(pozycja.Klucz, pozycja);
+        }
+
+        public void addZdarzenie(Zdarzenie zdarzenie)
+        {
+            dataContext.zdarzenie.Add(zdarzenie);
+        }
+
+        public void addOpisStanu(OpisStanu opisStanu)
+        {
+            dataContext.egzemplarze.Add(opisStanu);
+        }
+    }
+}
