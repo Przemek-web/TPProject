6d79e1cce7490c09b5dca07683a188bfd3cffadd
Przemyslaw Gornik <gornik.przemek@gmail.com>
2019-10-27 13:52:55 +0100

    Dodanie interfejsu Zdarzenie oraz klas Wypożyczenie i Oddanie

---
+++ Zadanie1/Oddanie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Oddanie : Zdarzenie
+    {
+        private DateTime dataOddania;
+        private Wykaz wykaz;
+        private OpisStanu opisStanu;
+
+        public Oddanie(DateTime dataOddania, Wykaz wykaz, OpisStanu opisStanu)
+        {
+            this.dataOddania = dataOddania;
+            this.wykaz = wykaz;
+            this.opisStanu = opisStanu;
+
+            this.opisStanu.CzyWypozyczona = false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Oddanie oddanie &&
+                   dataOddania == oddanie.dataOddania &&
+                   EqualityComparer<Wykaz>.Default.Equals(wykaz, oddanie.wykaz) &&
+                   EqualityComparer<OpisStanu>.Default.Equals(opisStanu, oddanie.opisStanu);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1511708615;
+            hashCode = hashCode * -1521134295 + dataOddania.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<Wykaz>.Default.GetHashCode(wykaz);
+            hashCode = hashCode * -1521134295 + EqualityComparer<OpisStanu>.Default.GetHashCode(opisStanu);
+            return hashCode;
+        }
+    }
+}
---
+++ Zadanie1/Wypozyczenie.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Wypozyczenie : Zdarzenie
+    {
+        private DateTime dataWypozyczenia;
+        private DateTime dataOddania;
+        private Wykaz wykaz;
+        private OpisStanu opisStanu;
+
+        public Wypozyczenie(DateTime dataWypozyczenia, DateTime dataOddania, Wykaz wykaz, OpisStanu opisStanu)
+        {
+            this.dataWypozyczenia = dataWypozyczenia;
+            this.dataOddania = dataOddania;
+            this.wykaz = wykaz;
+            this.opisStanu = opisStanu;
+
+            this.opisStanu.CzyWypozyczona = true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Wypozyczenie wypozyczenie &&
+                   dataWypozyczenia == wypozyczenie.dataWypozyczenia &&
+                   dataOddania == wypozyczenie.dataOddania &&
+                   EqualityComparer<Wykaz>.Default.Equals(wykaz, wypozyczenie.wykaz) &&
+                   EqualityComparer<OpisStanu>.Default.Equals(opisStanu, wypozyczenie.opisStanu);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 622673726;
+            hashCode = hashCode * -1521134295 + dataWypozyczenia.GetHashCode();
+            hashCode = hashCode * -1521134295 + dataOddania.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<Wykaz>.Default.GetHashCode(wykaz);
+            hashCode = hashCode * -1521134295 + EqualityComparer<OpisStanu>.Default.GetHashCode(opisStanu);
+            return hashCode;
+        }
+    }
+}
