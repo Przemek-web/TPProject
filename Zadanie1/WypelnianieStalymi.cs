1ea65e58dbfea0b1a931eb18a7ffc8db9c9fee9d
Przemyslaw Gornik <gornik.przemek@gmail.com>
2019-10-30 11:47:59 +0100

    Dodanie możliwosci testowania metod w aplikacji konsolowej

--- Zadanie1/WypelnianieStalymi.cs
+++ Zadanie1/WypelnianieStalymi.cs
@@ -20,14 +20,23 @@ namespace Zadanie1
             context.egzemplarze.Add(new OpisStanu(context.katalogi[1], false, "PT328"));
             context.egzemplarze.Add(new OpisStanu(context.katalogi[2], false, "WPIWP326"));
             context.egzemplarze.Add(new OpisStanu(context.katalogi[2], false, "WPIWP328"));
-            context.egzemplarze.Add(new OpisStanu(context.katalogi[3], true, "DZ64"));
+            context.egzemplarze.Add(new OpisStanu(context.katalogi[3], false, "DZ64"));
 
+            context.egzemplarze[0].CzyWypozyczona = true;
             context.zdarzenia.Add(new Wypozyczenie(new DateTime(2019, 5, 8), new DateTime(2019, 11, 12), context.czytelnicy[0], context.egzemplarze[0]));
+
+            context.egzemplarze[1].CzyWypozyczona = true;
             context.zdarzenia.Add(new Wypozyczenie(new DateTime(2019, 6, 9), new DateTime(2019, 11, 11), context.czytelnicy[0], context.egzemplarze[1]));
+
+            context.egzemplarze[1].CzyWypozyczona = false;
             context.zdarzenia.Add(new Oddanie(new DateTime(2019, 10, 10), context.czytelnicy[0], context.egzemplarze[1]));
 
+            context.egzemplarze[2].CzyWypozyczona = true;
             context.zdarzenia.Add(new Wypozyczenie(new DateTime(2019, 5, 5), new DateTime(2019, 11, 11), context.czytelnicy[1], context.egzemplarze[2]));
+
+            context.egzemplarze[2].CzyWypozyczona = false;
             context.zdarzenia.Add(new Oddanie(new DateTime(2019, 9, 9), context.czytelnicy[1], context.egzemplarze[2]));
+            
         }
     }
 }
