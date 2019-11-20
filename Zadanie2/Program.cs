43d77beb0548c69d9b5bf04d9c62c82a8e9c0387
Przemyslaw Gornik <gornik.przemek@gmail.com>
2019-11-20 06:51:25 +0100

    Utworzenie nowego projektu Zadanie2, utworzenie klasy Serialization i zaimplementowanie metod serializacji do formatu JSON

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadanie1;
+using Zadanie1.Tests;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static DataContext data = new DataContext();
+        static WypelnianieStalymi stale = new WypelnianieStalymi();
+        //static WypelnianieZPliku plik = new WypelnianieZPliku();
+        static DataRepository repository = new DataRepository(data, stale);
+        static DataService service = new DataService(repository);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aaaa");
+            repository.FillData();
+            // service.Wyswietl(service.WszystkiePozycjeWykazu());
+            // service.Wyswietl(service.WszystkiePozycjeOpisStanu());
+            //  service.Wyswietl(service.WszystkiePozycjeKatalogu());
+            //  service.Wyswietl(service.WszystkiePozycjeZdarzen());
+
+            Serialization serialization = new Serialization();
+            serialization.SerializeJSON(repository);
+
+
+        }
+    }
+}
---
+++ Zadanie2Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zadanie2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
