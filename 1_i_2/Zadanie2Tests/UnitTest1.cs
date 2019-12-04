033e75fa2aa31b543eb748e271d59e982b71bdc3
Przemyslaw Gornik <gornik.przemek@gmail.com>
2019-12-04 13:06:05 +0100

    Dodanie oraz zmiana testów jednostkowych

--- 1_i_2/Zadanie2Tests/UnitTest1.cs
+++ 1_i_2/Zadanie2Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Zadanie2;
 
@@ -10,28 +11,28 @@ namespace Zadanie2Tests
         [TestMethod]
         public void TestJSON()
         {
-            C c = new C(DateTime.Today, 14.32F, "tekstC", null);
-            B b = new B(DateTime.Today, 5.0F, "tekstB", c);
-            A a = new A(DateTime.Today, 3.14F, "tekstA", b);
-            c.A = a;
+            classC c = new classC(DateTime.Today, 14.32F, "tekstC", null);
+            classB b = new classB(DateTime.Today, 5.0F, "tekstB", c);
+            classA a = new classA(DateTime.Today, 3.14F, "tekstA", b);
+            c.aRef = a;
             SerializationJSON serializationJSON = new SerializationJSON();
             serializationJSON.SerializeJsonA(a, "A.json");
             serializationJSON.SerializeJsonB(b, "B.json");
             serializationJSON.SerializeJsonC(c, "C.json");
 
             DeserializationJSON deserializationJSON = new DeserializationJSON();
-            A a_new = deserializationJSON.DeserializeJsonA("A.json");
-            B b_new = deserializationJSON.DeserializeJsonB("B.json");
-            C c_new = deserializationJSON.DeserializeJsonC("C.json");
+            classA a_new = deserializationJSON.DeserializeJsonA("A.json");
+            classB b_new = deserializationJSON.DeserializeJsonB("B.json");
+            classC c_new = deserializationJSON.DeserializeJsonC("C.json");
 
             Assert.IsNotNull(a_new);
             Assert.IsNotNull(b_new);
             Assert.IsNotNull(c_new);
-            Assert.AreEqual(a.DateTimeA, a_new.DateTimeA);
-            Assert.AreEqual(b.Fb1, b_new.Fb1);
-            Assert.AreEqual(c.Sc1, c_new.Sc1);
-            Assert.AreEqual(a.Sa1, c.A.Sa1);
-            Assert.IsTrue(a == c.A);
+            Assert.AreEqual(a.dateTime, a_new.dateTime);
+            Assert.AreEqual(b.name, b_new.name);
+            Assert.AreEqual(c.number, c_new.number);
+            Assert.AreEqual(a.dateTime, c.aRef.dateTime);
+            Assert.IsTrue(a == c.aRef);
           
 
                 
@@ -44,43 +45,83 @@ namespace Zadanie2Tests
         [TestMethod]
         public void TestCSV()
         {
-            C c_a = new C(new DateTime(2019,2,3,1,1,1) , 14.32F, "tekstC", null);
-            B b_a = new B(new DateTime(2019, 2, 3,1,1,1), 5.0F, "tekstB", c_a);
-            A a_a = new A(new DateTime(2019, 2, 3, 1, 1, 1), 3.14F, "tekstA", b_a);
-            c_a.A = a_a;
-
-            SerializationCSV.SerializeA(a_a, "Atest.csv");
-
-            C c_b = new C(new DateTime(2019, 2, 3, 1, 1, 1), 14.32F, "tekstC", null);
-            B b_b = new B(new DateTime(2019, 2, 3, 1, 1, 1), 5.0F, "tekstB", c_b);
-            A a_b = new A(new DateTime(2019, 2, 3, 1, 1, 1), 3.14F, "tekstA", b_b);
-            c_b.A = a_b;
-            SerializationCSV.SerializeB(b_b, "Btest.csv");
+            classC c_a = new classC(new DateTime(2019,2,3,1,1,0) , 14.32F, "tekstC", null);
+            classB b_a = new classB(new DateTime(2019, 2, 3,1,1,0), 5.0F, "tekstB", c_a);
+            classA a_a = new classA(new DateTime(2019, 2, 3, 1, 1, 0), 3.14F, "tekstA", b_a);
+            c_a.aRef = a_a;
 
 
-            C c_c = new C(new DateTime(2019, 2, 3, 1, 1, 1), 14.32F, "tekstC", null);
-            B b_c = new B(new DateTime(2019, 2, 3, 1, 1, 1), 5.0F, "tekstB", c_c);
-            A a_c = new A(new DateTime(2019, 2, 3, 1, 1, 1), 3.14F, "tekstA", b_c);
-            c_c.A = a_c;
-            SerializationCSV.SerializeC(c_c, "Ctest.csv");
+            FormatterCSV formatterCSVA = new FormatterCSV();
 
-            A newa = DeserializationCSV.DeserializeA("Atest.csv");
-            B newb = DeserializationCSV.DeserializeB("Btest.csv");
-            C newc = DeserializationCSV.DeserializeC("Ctest.csv");
+            using (FileStream stream = File.Open("A.csv", FileMode.OpenOrCreate))
+            {
+                formatterCSVA.Serialize(stream, a_a);
+            }
+
+
+
+            classC c_b = new classC(new DateTime(2019, 2, 3, 1, 1, 0), 14.32F, "tekstC", null);
+            classB b_b = new classB(new DateTime(2019, 2, 3, 1, 1, 0), 5.0F, "tekstB", c_b);
+            classA a_b = new classA(new DateTime(2019, 2, 3, 1, 1, 0), 3.14F, "tekstA", b_b);
+            c_b.aRef = a_b;
+          
+            FormatterCSV formatterCSVB = new FormatterCSV();
+
+            using (FileStream stream = File.Open("B.csv", FileMode.OpenOrCreate))
+            {
+                formatterCSVB.Serialize(stream, b_b);
+            }
+
+
+              classC c_c = new classC(new DateTime(2019, 2, 3, 1, 1, 0), 14.32F, "tekstC", null);
+              classB b_c = new classB(new DateTime(2019, 2, 3, 1, 1, 0), 5.0F, "tekstB", c_c);
+              classA a_c = new classA(new DateTime(2019, 2, 3, 1, 1, 0), 3.14F, "tekstA", b_c);
+              c_c.aRef = a_c;
+      
+            FormatterCSV formatterCSVC = new FormatterCSV();
+
+            using (FileStream stream = File.Open("C.csv", FileMode.OpenOrCreate))
+            {
+                formatterCSVC.Serialize(stream, c_c);
+            }
+
+             classA newa;
+
+             using (FileStream stream = File.Open("A.csv", FileMode.Open))
+            {
+                newa = (classA)formatterCSVC.Deserialize(stream);
+            }
+
+
+             classB newb;
+            
+
+            using (FileStream stream = File.Open("B.csv", FileMode.Open))
+            {
+                newb = (classB)formatterCSVB.Deserialize(stream);
+            }
+ 
+
+            classC newc;
+
+            using (FileStream stream = File.Open("C.csv", FileMode.Open))
+            {
+                newc = (classC)formatterCSVC.Deserialize(stream);
+            }
             Assert.IsNotNull(newa);
-            Assert.IsNotNull(newb);
-            Assert.IsNotNull(newc);
+              Assert.IsNotNull(newb);
+              Assert.IsNotNull(newc);
 
 
-            Assert.IsTrue(newa == newa.B.C.A);
-            Assert.IsTrue(newb == newb.C.A.B);
-            Assert.IsTrue(newc == newc.A.B.C);
-            Assert.IsTrue(newc.Equals(newc.A.B.C));
+              Assert.IsTrue(newa == newa.bRef.cRef.aRef);
+              Assert.IsTrue(newb == newb.cRef.aRef.bRef);
+              Assert.IsTrue(newc == newc.aRef.bRef.cRef);
+              Assert.IsTrue(newc.Equals(newc.aRef.bRef.cRef));
 
 
-            Assert.AreEqual(newa.Fa1, a_a.Fa1);
-            Assert.AreEqual(newa.Sa1, a_a.Sa1);
-            Assert.AreEqual(newa.DateTimeA.Date, a_a.DateTimeA.Date);
+              Assert.AreEqual(newa.number, a_a.number);
+              Assert.AreEqual(newa.name, a_a.name);
+              Assert.AreEqual(newa.dateTime, a_a.dateTime); 
 
         }
 
