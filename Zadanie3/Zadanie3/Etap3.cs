531521916bdd092163605822c82f1e58f7ada975
Przemyslaw Gornik <gornik.przemek@gmail.com>
2019-12-11 15:09:54 +0100

    Wstepna inicjalizacja, sprawdzenie lacznosci, mapowania, pierwsza metoda i test z etapu3

---
+++ Zadanie3/Zadanie3/Etap3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public class Etap3
+    {
+
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            DataClassesDataContext db = new DataClassesDataContext();
+            List<Product> query =
+                (from p in db.Product
+                 where SqlMethods.Like(p.Name, "%" + namePart + "%")
+                 select p).ToList();
+
+            return query;
+        }
+    }
+}
---
+++ Zadanie3/Zadanie3Test/Etap3Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie3;
+
+namespace Zadanie3Test
+{
+    [TestClass]
+    public class Etap3Test
+    {
+        
+        
+        [TestMethod()]
+        public void GetProductsByNameTest()
+        {
+            DataClassesDataContext db = new DataClassesDataContext();
+            List<Product> query = Etap3.GetProductsByName("Decal");
+
+            List<Product> check = new List<Product>();
+            Product C = (from p in db.Product where p.ProductID == 325 select p).First();
+            check.Add(C);
+            C = (from p in db.Product where p.ProductID == 326 select p).First();
+            check.Add(C);
+
+            for (int i = 0; i < query.Count(); i++)
+            {
+                if (query[i].ProductID != check[i].ProductID) Assert.Fail();
+                if (query[i].Name != check[i].Name) Assert.Fail();
+            }
+            if (query.Count() != check.Count()) Assert.Fail();
+        }
+    }
+}
