06d1d273203c256fc0bd4c9e4d5ff74b40ed6a27
Krystian <k.domdalski@Onet.pl>
2020-01-19 12:34:57 +0100

    Revert "Revert "Added DataLayer""
    This reverts commit 9a52db553d41fc95e2973a67a5b29be53c44582b.

---
+++ 4/DataLayer/Etap4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public static class Etap4
+    {
+        public static List<Product> produktyBezKategoriiFROM(this List<Product> lista)
+        {
+            List<Product> wynik = (from produkt in lista
+                                   where produkt.ProductSubcategoryID == null
+                                   select produkt).ToList();
+            return wynik;
+        }
+
+        public static List<Product> produktyBezKategoriiROZSZERZAJĄCA(this List<Product> lista)
+        {
+            List<Product> wynik;
+            wynik = lista.Where(produkt => produkt.ProductSubcategoryID == null).ToList();
+            return wynik;
+        }
+
+        public static String LancuchZnakowROZSZERZAJĄCA(this List<Product> produkty, List<ProductVendor> dostawcy)
+        {
+
+            String s = "";
+            var zapytanie = produkty.Join(dostawcy, produkt => produkt.ProductID, dostawca => dostawca.ProductID,
+            (produkt, dostawca) => produkt.Name + " - " + dostawca.Vendor.Name + "\n").ToList();
+
+            foreach (var z in zapytanie)
+            {
+                s = s + z.ToString();
+            }
+            return s;
+        }
+
+        public static List<List<Product>> PodzielNaStrony(this List<Product> produkty, int liczbaProduktow, int liczbaStron)
+        {
+            List<List<Product>> strony = new List<List<Product>>();
+            List<Product> strona = new List<Product>();
+            for (int i = 0; i < liczbaStron; i++)
+            {
+                strona = produkty.Skip(liczbaProduktow * i).Take(liczbaProduktow).ToList();
+                strony.Add(strona);
+            }
+            return strony;
+        }
+    }
+}
---
+++ 4/UnitTests/Etap4Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataLayer;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Etap4Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataClassesDataContext dataContext = new DataClassesDataContext();
+            List<Product> produkty = dataContext.Products.ToList();
+            Assert.AreEqual(produkty.produktyBezKategoriiROZSZERZAJĄCA().Count, produkty.produktyBezKategoriiFROM().Count);
+            Assert.AreEqual(Etap4.PodzielNaStrony(produkty, 5, 10)[0].Count, 5);
+        }
+    }
+}
